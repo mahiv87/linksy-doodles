a3d7afd3deda4fdf97c645707d6aaf4bac950a86
mahiv87 <maherreraiv1987@gmail.com>
2024-03-17 11:24:55 -0600

    refactor Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from 'react';
 import './App.css';
 import CardComponent from './components/Card/CardComponent';
+import avatar from './assets/images/avatar-jessica.jpeg';
 
 function App() {
+	const user = {
+		avatar: avatar,
+		name: 'Jessica Randall',
+		location: 'London, United Kingdom',
+		headline: 'Front-end developer and avid reader.',
+		links: ['GitHub', 'Frontend Mentor', 'LinkedIn', 'Twitter', 'Instagram']
+	};
+
 	return (
 		<>
-			<CardComponent />
+			<CardComponent
+				avatar={user.avatar}
+				name={user.name}
+				location={user.location}
+				headline={user.headline}
+				links={user.links}
+			/>
 
 			{/* <div className="attribution">
 				Challenge by{' '}
--- src/components/Card/CardComponent.jsx
+++ src/components/Card/CardComponent.jsx
@@ -1,15 +1,15 @@
 import ButtonComponent from '../Button/ButtonComponent';
 import styles from './CardComponent.module.css';
-import avatar from '../../assets/images/avatar-jessica.jpeg';
+// import avatar from '../../assets/images/avatar-jessica.jpeg';
 
-function CardComponent() {
+function CardComponent({ avatar, name, location, headline, links }) {
 	return (
 		<div className={styles.card}>
 			<img src={avatar} alt="Profile avatar" className={styles.avatar} />
-			<h1 className={styles.name}>Jessica Randall</h1>
-			<h4 className={styles.location}>London, United Kingdom</h4>
-			<p>"Front-end developer and avid reader."</p>
-			<ButtonComponent />
+			<h1 className={styles.name}>{name}</h1>
+			<h4 className={styles.location}>{location}</h4>
+			<p>"{headline}"</p>
+			<ButtonComponent link />
 		</div>
 	);
 }
