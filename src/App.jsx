cf6ad3d4c30f86ae6a8e39b51f17fe13135ed188
mahiv87 <maherreraiv1987@gmail.com>
2024-03-17 09:51:27 -0600

    fixed ref link for assets

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,25 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<div>
+				Jessica Randall London, United Kingdom "Front-end developer and avid
+				reader." GitHub Frontend Mentor LinkedIn Twitter Instagram
+			</div>
+			<div className="attribution">
+				Challenge by{' '}
+				<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+					Frontend Mentor
+				</a>
+				. Coded by{' '}
+				<a href="https://github.com/mahiv87/linksy-doodles" target="_blank">
+					Marcus Herrera
+				</a>
+				.
+			</div>
+		</>
+	);
 }
 
 export default App;
